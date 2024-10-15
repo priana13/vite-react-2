5dec0e033bf3298686874512180e4c62d861cee6
priana13 <herian.sap@gmail.com>
2024-10-15 20:38:03 +0700

    install vite react

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+function App() { 
+
+  return (
+    <> 
+      <div className="text-center min-h-screen bg-blue-200 flex items-center justify-center">
+
+        <h1 className="text-2xl">Priana Saputra</h1>
+
+      </div>   
+     
+     
+    </>
+  )
+}
+
+export default App
