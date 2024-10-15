9509be172df8ee4997256616036c49fe54a34c2d
priana13 <herian.sap@gmail.com>
2024-10-15 21:05:33 +0700

    pengenalan component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
+import React from "react"
+
+const Tombol = (props) => {
+
+  const {children = 'Tombol', color} = props;
+
+  return (<button type="button" className={`text-white bg-${color}-700 hover:bg-${color}-800 focus:ring-4 focus:ring-${color}-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-${color}-600 dark:hover:bg-${color}-700 focus:outline-none dark:focus:ring-${color}-800`}>
+    {children}
+  </button>)
+}
+
+
 
 function App() { 
 
+
   return (
     <> 
-      <div className="text-center min-h-screen bg-blue-200 flex items-center justify-center">
+      <div className="text-center min-h-screen bg-blue-200 pt-6">
 
         <h1 className="text-2xl">Priana Saputra</h1>
 
-      </div>   
+        <br />
+
+      
+
+       <Tombol color="red" >
+          Klik Saya
+       </Tombol>
+       <Tombol color="blue">
+          Klik Saya
+       </Tombol>
+       
+       <Tombol color="blue" />
+      
+
+      </div>  
+
+     
      
      
     </>
