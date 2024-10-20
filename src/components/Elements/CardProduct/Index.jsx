5ba7ba99acedf64677dff7850c6b64f48490d854
priana13 <herian.sap@gmail.com>
2024-10-20 20:08:42 +0700

    nested component

--- src/components/Elements/CardProduct/Index.jsx
+++ src/components/Elements/CardProduct/Index.jsx
@@ -1,27 +1,63 @@
 import Tombol from "../Tombol/Index";
 
 
-const CardProduct = () => {
+const CardProduct = (props) => {
+
+    const {children} = props;
 
     return (
-        <>
-            
+        <>            
             <div className="w-[200px] max-w-sm bg-white border border-gray-200 rounded-lg shadow p-2 hover:scale-105 duration-100">
-                <a href="" className="">                       
-                    <img src="/sepatu.jpeg" alt="" className="rounded-t-lg w-full" />
-                </a>
-
-                <p className="my-4">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-
-                <h2 className="text-2xl font-bold my-22">Rp. 1000.000</h2>
-
-                <div className="text-center my-3">
-                    <Tombol bg="bg-blue-500 hover:bg-blue-700">Add To Cart</Tombol>                   
-                </div>                   
+              
+              {children}
 
             </div>
         </>
     )
 }
+
+
+const Image = (props) => {
+
+    const {image} = props;
+
+    return (
+      <a href="" className="">                       
+        <img src={image} alt="" className="rounded-t-lg w-full" />
+       </a>
+    )
+}
+
+const Description = (props) => {
+
+    const {children , title} = props;
+
+    return (
+        <>
+            <h3 className="text-xl font-bold mt-2">{title}</h3>
+            <p className="my-4">{children}</p>
+        </>
+    )
+}
+
+const Footer = (props) => {
+
+    const {price} = props;
+
+    return (
+    <>
+        <h2 className="text-2xl font-bold my-22">{price}</h2>
+
+        <div className="text-center my-3">
+            <Tombol bg="bg-blue-500 hover:bg-blue-700">Add To Cart</Tombol>                   
+        </div> 
+
+    </>
+    )
+}
+
+CardProduct.Image = Image;
+CardProduct.Description = Description;
+CardProduct.Footer = Footer;
 
 export default CardProduct;
--- src/components/Pages/products.jsx
+++ src/components/Pages/products.jsx
@@ -6,11 +6,30 @@ const ProductPage = () => {
     return (
         <>
             <div className="flex justify-center gap-2"> 
-              <CardProduct />
+              <CardProduct >
+                 <CardProduct.Image image="/sepatu.jpeg" />
+                 <CardProduct.Description   title="Sepatu Olahraga 1"  >
+                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                 </CardProduct.Description>
+                 <CardProduct.Footer price="Rp. 500.000" />
+              </CardProduct>
 
-              <CardProduct />
 
-              <CardProduct />
+              <CardProduct >
+                 <CardProduct.Image image="/sepatu.jpeg" />
+                 <CardProduct.Description title="Sepatu Orahraga 2" >
+                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                 </CardProduct.Description>
+                 <CardProduct.Footer price="Rp. 400.000" />
+              </CardProduct>
+              
+              <CardProduct >
+                 <CardProduct.Image image="/sepatu.jpeg" />
+                 <CardProduct.Description title="Sepatu Orahraga 2" >
+                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                 </CardProduct.Description>
+                 <CardProduct.Footer price="Rp. 600.000" />
+              </CardProduct>
               
             </div>           
         </>
