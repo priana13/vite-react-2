29e2c1f15eb668fdeb40013b51b789a3e3953ed5
priana13 <herian.sap@gmail.com>
2024-11-02 23:07:49 +0700

    penggunaan use content untuk fiture dakmode

--- src/components/Layouts/AuthLayouts.jsx
+++ src/components/Layouts/AuthLayouts.jsx
@@ -1,31 +1,46 @@
 import { Link } from "react-router-dom";
 import FormLogin from "../Fragments/FormLogin";
+import { useContext } from "react";
+import { DarkMode } from "../../context/DarkMode";
 
 const AuthLayouts = (props) => {
     const {children , title , type} = props;
+
+    const {isDarkMode, setIsDarkMode} = useContext(DarkMode);
+
+    console.log(isDarkMode)
+
     return (
-    
-        <div className="w-[80%] sm:w-[30%] mx-auto rounded-lg p-6">
+        <div className={`${isDarkMode && "bg-slate-800"} h-screen flex justify-items-center`}>
 
-          <h1 className="text-3xl font-bold mb-2 text-center">{title}</h1>   
-          <p className="font-medium font-slate-500 text-center">Please Enter youre detail</p>
+            <button className="bg-blue-500 text-white p-2 absolute rounded-lg right-0" onClick={() => setIsDarkMode( !isDarkMode )}>Dark</button>
 
-            {children}  
-       
-            <p> 
-                {type === 'login' ? "Don't have an account? " : "Already have an account? "} 
-                
-                {type === 'login' && (
-                    <Link className="text-blue-700" to="/register">Register Now</Link>
-                )}
+            <div className={`w-[80%] sm:w-[50%] mx-auto rounded-lg p-6 my-auto ${isDarkMode && "bg-white"} ${!isDarkMode && "bg-blue-100"}`}>
 
-                {type === 'register' && (
-                    <Link className="text-blue-700" to="/login">Login</Link>
-                )}
-                
-            </p>
+                <h1 className="text-3xl font-bold mb-2 text-center">{title}</h1>   
+                <p className="font-medium font-slate-500 text-center">Please Enter youre detail</p>
+
+                {children}  
+
+                <p> 
+                    {type === 'login' ? "Don't have an account? " : "Already have an account? "} 
+                    
+                    {type === 'login' && (
+                        <Link className="text-blue-700" to="/register">Register Now</Link>
+                    )}
+
+                    {type === 'register' && (
+                        <Link className="text-blue-700" to="/login">Login</Link>
+                    )}
+                    
+                </p>
+
+            </div>
+
 
         </div>
+    
+        
     )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import ProductPage from './components/Pages/products.jsx';
 import ProductDetailPage from './components/Pages/productDetail.jsx';
 import { Provider } from 'react-redux';
 import store from "./redux/store.js";
+import DarkModeContextProvider from './context/DarkMode.jsx';
 
 
 const router = createBrowserRouter([
@@ -46,7 +47,13 @@ createRoot(document.getElementById('root')).render(
     {/* <App /> */}
     <Provider store={store} >
 
-      <RouterProvider router={router} />
+      {/* <RouterProvider router={router} /> */}
+
+      <DarkModeContextProvider>
+
+        <RouterProvider router={router} />
+        
+      </DarkModeContextProvider>
 
     </Provider>
   </StrictMode>,
